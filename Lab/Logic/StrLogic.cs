cc90c0f0eae24d7ad3d41d6e9d1bce70c435b0e2
bokutm <bokutm@mail.ru>
2020-01-21 14:59:47 +0300

    Labs done

---
+++ Lab/Logic/StrLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public static class StrLogic
+    {
+        public static string ReverceString(this string str)
+        {
+            StringBuilder sb = new StringBuilder(str.Length);
+            for (int i = str.Length; i-- != 0;)
+                sb.Append(str[i]);
+            return sb.ToString();
+        } 
+
+        public static string firstLatterBig(this string str)
+        {
+            var sb = new StringBuilder(str.Length);
+            bool inWord = false;
+            foreach (var c in str)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (!inWord)
+                        sb.Append(char.ToUpper(c));
+                    else
+                        sb.Append(c);
+                    inWord = true;
+                }
+                else
+                {
+                    sb.Append(c);
+                    inWord = false;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
